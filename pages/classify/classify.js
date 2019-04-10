d125a2c92bc41932e909dd453293a18c3cd44a76
unknown <unknown@example.com>
2019-04-10 09:57:41 +0800

    首页完成

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -5,62 +5,100 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    classify: [{
+      name: "女士",
+      picUrl: "",
+      list: [{
+        name: "服饰",
+        list: [
+          "全部", "上衣", "外套", "连衣裙", "下装"
+        ]
+      }, {
+        name: "手袋",
+        list: [
+          "全部", "手提包", "单肩包", "斜挎包", "托特包", "双肩背包", "钱夹", "小皮件"
+        ]
+      }, {
+        name: "黑白系列",
+        list: [
+          "全部", "上衣", "外套", "连衣裙", "下装"
+        ]
+      }]
+    }, {
+      name: "男士",
+      picUrl: "",
+      list: [{
+        name: "服饰",
+        list: [
+          "全部", "上衣", "外套", "连衣裙", "下装"
+        ]
+      }, {
+        name: "手袋",
+        list: [
+          "全部", "手提包", "单肩包", "斜挎包", "托特包", "双肩背包", "钱夹", "小皮件"
+        ]
+      }, {
+        name: "黑白系列",
+        list: [
+          
+        ]
+      }]
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,19 +4,127 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
+    motto: 'MICHAEL KORS 早春系列全新上市。',
+    /*userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo')*/
+    searchShow: false,
+    imgUrls: [{
+      id: 0,
+      url: '../../img/mob-img_01.jpg',
+      txt: '文字介绍1',
+      linkUrl: ''
+    },
+    {
+      id: 1,
+      url: '../../img/mob-img_02.jpg',
+      txt: '文字介绍2',
+      linkUrl: ''
+    },
+    {
+      id: 2,
+      url: '../../img/mob-img_03.jpg',
+      txt: '文字介绍3',
+      linkUrl: ''
+    }
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 300,
+    series: [{
+      id: 0,
+      picUrl: '../../img/mob-img_05.jpg',
+      txt: '女士新品系列',
+      linkUrl: ''
+    },
+    {
+      id: 1,
+      picUrl: '../../img/mob-img_04.jpg',
+      txt: '男士新品系列',
+      linkUrl: ''
+    }
+    ],
+    proList: [{
+      id: 0,
+      num: 0,
+      picUrl: ['../../img/pro01.jpg'],
+      name: 'Whitney 中号蝴蝶印花皮质手提包',
+      price: '￥5300',
+      classify: [{
+        id: '0',
+        picUrl: '../../img/pro_classify_01.jpg'
+      }]
+    },
+    {
+      id: 1,
+      num: 0,
+      picUrl: ['../../img/pro02.jpg'],
+      name: 'Whitney 大号皮质单肩包',
+      price: '￥5300',
+      classify: [{
+        id: '0',
+        picUrl: '../../img/pro_classify_02.jpg'
+      }]
+    },
+    {
+      id: 2,
+      num: 0,
+      picUrl: ['../../img/pro03.jpg'],
+      name: 'Whitney 大号皮质单肩包',
+      price: '￥4600',
+      classify: [{
+        id: '0',
+        picUrl: '../../img/pro_classify_03.jpg'
+      }]
+    },
+    {
+      id: 3,
+      num: 0,
+      picUrl: ['../../img/pro04.jpg', '../../img/pro04-2.jpg'],
+      name: 'Whitney 大号皮质单肩包',
+      price: '￥3400',
+      classify: [{
+        id: '0',
+        picUrl: '../../img/pro_classify_04-1.jpg'
+      },
+      {
+        id: '1',
+        picUrl: '../../img/pro_classify_04-2.jpg'
+      }
+      ]
+    }
+    ]
+  },
+  showSearch: function () {
+    var that = this;
+    if (this.data.searchShow == false) {
+      that.setData({
+        searchShow: true
+      })
+    }else{
+      that.setData({
+        searchShow: false
+      })
+    }
+  },
+  switchTab: function (e) {
+    let dataNum = e.currentTarget.dataset.num;
+    let index = parseInt(e.currentTarget.dataset.id);
+    let newNum = 'proList[' + index + '].num';
+    var that = this;
+    that.setData({
+      [newNum]: dataNum
+    })
   },
   //事件处理函数
-  bindViewTap: function() {
+  /*bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
-  },
+  },*/
   onLoad: function () {
-    if (app.globalData.userInfo) {
+    /*if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -41,14 +149,14 @@ Page({
           })
         }
       })
-    }
+    }*/
   },
-  getUserInfo: function(e) {
+  /*getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  }*/
 })
